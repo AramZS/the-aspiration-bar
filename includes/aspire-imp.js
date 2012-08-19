94868ab0ad4b227fbd58ee3541b2552ba3e58328
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-08-19 01:24:21 -0400

    trying to get jquery working

--- includes/aspire-imp.js
+++ includes/aspire-imp.js
@@ -4,22 +4,23 @@ jQuery(document).ready(function() {
 		// validate and process form here 
 		// via http://stackoverflow.com/questions/6844952/execute-function-when-enter-is-pressed-if-textbox-has-focus
 		if ( e.keyCode === 13 ) {
+			jQuery(function() {  
 			var aspirationEntry = jQuery("input#aspiration-entry").val();
 			//test
 			//alert (dataString);return false;  
 			var dataString = 'aspiration-entry='+ aspirationEntry;
 			jQuery.ajax({
 			
-				type: 'POST',
+				//type: 'POST',
 				action: 'tabarAjaxery',
-				url: 'admin-ajax.php',
+				//url: 'admin-ajax.php',
 				data: dataString,
 				success: function() {
 					jQuery('.tabar-loop').prepend('<h5 class="aspiration-title">' + aspirationEntry + '</h5>');
 				}
 			
 			});
-			
+			});
 		}
 	  });  
 	});  
